7a11b6c72523b638f654768047643fcad9b4c56c
AbigailRojasChampa <125307444+AbigailRojasChampa@users.noreply.github.com>
2024-03-27 02:14:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,27 @@ app = Flask (__name__)
 @app.route("/")
 def portada():
       return '''
-        <!DOCTYPE html>
-        <html lang="es">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>MI PRESENTACION</title>
-        </head>
-        <body>
-            <h1>MI PRESENTACION</h1>
-            <img src="https://cdn.icon-icons.com/icons2/2859/PNG/512/avatar_face_man_boy_profile_smiley_happy_people_icon_181659.png" alt="Descripción de la imagen">
-            <p>Nombre: Nombre del usuario</p>
-        </body>
-        </html>
+     <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos de Maquillaje</title>
+</head>
+<body>
+    <header>
+        <h1>Tipos de Maquillaje</h1>
+    </header>
+    <main>
+        <section>
+            <h2>Maquillaje</h2>
+            <img src="https://elcomercio.pe/resizer/baG32pMFY-rnpgNfZwezvZ0TRSo=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/SVSVLNI6KBAZDJTYROSRRHWTTY.png" alt="Descripción de la imagen">
+            <p>Nombre: maquillaje</p>
+        </section>
+    </main>
+</body>
+</html>
+
       '''
 if __name__ == "__main__":
        app,run(debug=True)
