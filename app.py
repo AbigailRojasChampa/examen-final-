713c67d592c54869abad3143627a8dd7555a7afe
AbigailRojasChampa <125307444+AbigailRojasChampa@users.noreply.github.com>
2024-03-27 02:18:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask (__name__)
 @app.route("/")
 def portada():
       return '''
-    <!DOCTYPE html>
+   <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
@@ -14,7 +14,7 @@ def portada():
     <style>
         body {
             font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
+            background-color: #f9f9f9;
             margin: 0;
             padding: 0;
         }
@@ -22,43 +22,42 @@ def portada():
             background-color: #ffcccc;
             padding: 20px;
             text-align: center;
-        }
-        h1 {
-            margin: 0;
             color: #333;
         }
         main {
             padding: 20px;
+            text-align: center;
         }
         section {
             background-color: #fff;
             border-radius: 10px;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
             padding: 20px;
             margin-bottom: 20px;
         }
         img {
             max-width: 100%;
-            height: auto;
-            display: block;
-            margin: 0 auto 20px;
-            border-radius: 5px;
+            border-radius: 10px;
+            margin-bottom: 10px;
         }
         p {
-            margin: 0;
-            color: #666;
+            font-size: 18px;
+            color: #555;
+        }
+        h2 {
+            color: #ff6699;
         }
     </style>
 </head>
 <body>
     <header>
-        <h1>Tipos de Maquillaje</h1>
+        <h1>¡Descubre los Mejores Tipos de Maquillaje!</h1>
     </header>
     <main>
         <section>
             <h2>Maquillaje</h2>
             <img src="https://elcomercio.pe/resizer/baG32pMFY-rnpgNfZwezvZ0TRSo=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/SVSVLNI6KBAZDJTYROSRRHWTTY.png" alt="Maquillaje">
-            <p>El maquillaje es una forma de arte que nos permite resaltar la belleza natural del rostro.</p>
+            <p>¡Experimenta con diferentes estilos y tendencias!</p>
         </section>
     </main>
 </body>
